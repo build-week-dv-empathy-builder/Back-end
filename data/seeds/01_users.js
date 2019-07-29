f60cda8ddd048b3bc75a000e566fd9d4bd75d66f
Derrick Mei <derrick.mei.168@gmail.com>
2019-07-29 16:34:53 -0500

    fixed seed data
    added 3 users seed
    
    added 1 user data

---
+++ data/seeds/01_users.js
@@ -0,0 +1,27 @@
+const generatePassword = require('../../api/functions/generatePassword');
+
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex('users')
+        .del()
+        .then(function() {
+            // Inserts seed entries
+            return knex('users').insert([
+                {
+                    id: 1,
+                    email: 'jj@gmail.com',
+                    password: generatePassword('password')
+                },
+                {
+                    id: 2,
+                    email: 'tb@gmail.com',
+                    password: generatePassword('password')
+                },
+                {
+                    id: 3,
+                    email: 'hj@gmail.com',
+                    password: generatePassword('password')
+                }
+            ]);
+        });
+};
